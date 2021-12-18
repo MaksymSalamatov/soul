3dadd0fe0f6f3bce511107df01bd490533094fa1
MaksymSalamatov <78314973+MaksymSalamatov@users.noreply.github.com>
2021-12-18 06:34:13 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,44 +1,48 @@
-const burger = document.querySelector('.burger'),
-      reference = document.querySelectorAll('.header__link'),
-      menu = document.querySelector('.nav'),
-      overlay = document.querySelector('.first-screen__overlay'),
-      popapClose = document.querySelector('.popap__close'),
-      popapArea = document.querySelector('.popap__area'),
-      btnPopup = document.querySelector('.btnPopap'),
-      popup = document.querySelector('.popap'),
-      body = document.querySelector('body');
-function toggle() {
-    menu.classList.toggle('show');
-    burger.classList.toggle('show');
-    overlay.classList.toggle('show');
-    body.classList.toggle('stop-scroll');
+
+function clickedBurger(){
+    const burger = document.querySelector('.burger'),
+        reference = document.querySelectorAll('.header__link'),
+        menu = document.querySelector('.nav'),
+        overlay = document.querySelector('.first-screen__overlay'),
+        popapClose = document.querySelector('.popap__close'),
+        popapArea = document.querySelector('.popap__area'),
+        btnPopup = document.querySelector('.btnPopap'),
+        popup = document.querySelector('.popap'),
+        body = document.querySelector('body');
+    function toggle() {
+        menu.classList.toggle('show');
+        burger.classList.toggle('show');
+        overlay.classList.toggle('show');
+        body.classList.toggle('stop-scroll');
+    }
+    function toggleSecond() {
+        popapClose.classList.toggle('showIs');
+        popup.classList.toggle('showIs');
+        popapArea.classList.toggle('showIs');
+        body.classList.toggle('stop-scroll');
+    }
+    function toggleRemove () {
+        overlay.classList.remove('show');
+        burger.classList.toggle('show');
+        menu.classList.toggle('show');
+        body.classList.remove('stop-scroll');
+    }
+    function listener(list, event = 'click', callback) {
+        list.addEventListener( event, callback);
+    }
+    listener(overlay, 'click', toggle);
+    listener(burger, 'click', toggle);
+    listener(btnPopup, 'click', toggleSecond);
+    listener(popapArea,'click', toggleSecond);
+    listener(popapClose,'click', toggleSecond);
+    listener(body, 'click', function (e) {
+        if(e.target.classList.contains('header-overlay')) toggle();
+    });
+    reference.forEach(item => {
+        item.addEventListener('click', toggleRemove);
+    });
 }
-function toggleSecond() {
-    popapClose.classList.toggle('showIs');
-    popup.classList.toggle('showIs');
-    popapArea.classList.toggle('showIs');
-    body.classList.toggle('stop-scroll');
-}
-function toggleRemove () {
-    overlay.classList.remove('show');
-    burger.classList.toggle('show');
-    menu.classList.toggle('show');
-    body.classList.remove('stop-scroll');
-}
-function listener(list, event = 'click', callback) {
-    list.addEventListener( event, callback);
-}
-listener(overlay, 'click', toggle);
-listener(burger, 'click', toggle);
-listener(btnPopup, 'click', toggleSecond);
-listener(popapArea,'click', toggleSecond);
-listener(popapClose,'click', toggleSecond);
-listener(body, 'click', function (e) {
-    if(e.target.classList.contains('header-overlay')) toggle();
-});
-reference.forEach(item => {
-   item.addEventListener('click', toggleRemove);
-});
+clickedBurger()
 
 
 
